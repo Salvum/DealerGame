5c98ad483c42029ae8a7875e1c2304e8015c9052
Salvum <osintserg@gmail.com>
2016-10-23 13:11:39 +0500

    Salvum - V 0.004

--- js/Drugdealer.js
+++ js/Drugdealer.js
@@ -3,30 +3,61 @@ var model = {
     boardWidthSize: 4,
     boardHeightSize: 9,
     numDrugDealers: 3,
-    shipLength: 3,
-    shipsSunk: 0,
-    drugDealers: [{ locations: ["05", "06", "07"], hits: ["", "", ""] },
-        { locations: ["15", "25", "35"], hits: ["", "", ""] },
-        { locations: ["33", "23", "22"], hits: ["", "", ""] }
+    drugDealersLine: 3,
+    deathDrugDealer: 0,
+    timeToLose: 30,
+    drugDealers:
+        [{ locations: ["05", "06", "07"], hits: ["", "", ""] },
+         { locations: ["15", "25", "35"], hits: ["", "", ""] },
+         { locations: ["33", "23", "22"], hits: ["", "", ""] }
     ],
-    fire: function(guess){
-        for (var = 0; i < this.numDealers; i++){
+    fire : function ( guess ){
+        for (var i = 0; i < this.numDrugDealers; i++){
             var drugDealer = this.drugDealers[i];
-            locations = ship.locations;
+            var index = drugDealer.locations.indexOf(guess);
+            if (index >= 0) {
+                drugDealer.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("Барыга уничтожен!");
+                if (this.isDead(drugDealer)) {
+                    view.displayMessage("Я уничтожил один из отрядов барыг!");
+                    this.deathDrugDealer++;
+                }
+                return true;
+            }
         }
+        view.displayMiss(guess);
+        view.displayMessage("Я промахнулся");
+        return false;
+},
+isDead: function(drugDealer) {
+    for (var i = 0; i < this.drugDealersLine; i++) {
+        if (drugDealer.hits[i] !== "hit") {
+            return false;
+        }
+    }
+    return true;
 }
 };
 var view = {
-    displayMessage: function (msg) {
+    displayMessage: function ( msg ) {
         var messageMavashi = document.getElementById("messageMavashi");
-        messageMavashi.innerHTML ="KoKo";
+        messageMavashi.innerHTML ="";
     },
-    displayHit: function (location) {
+    displayHit: function ( location ) {
         var cell = document.getElementById(location);
-            cell.setAttribute("class", "hit");
+        cell.setAttribute("class", "hit");
     },
-    displayMiss: function (location) {
+    displayMiss: function ( location ) {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
 };
+model.fire("00");
+model.fire("11");
+model.fire("12");
+model.fire("33");
+model.fire("34");
+model.fire("21");
+model.fire("05");
+model.fire("02");
--- js/instructions.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-function Duck(sound) {
-    this.sound = sound;
-    this.quack = function() {console.log(this.sound);}
-}
-var toy = new Duck("quack quack");
-toy.quack();
-console.log(typeof toy);
-console.log(toy instanceof Duck);
